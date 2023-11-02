b8bdf4978e20c5e48939ed56b411938a08c7c4fd
owencooke <ocooke@ualberta.ca>
2023-11-02 00:07:20 -0600

    Added project colors to theme. Started working on NavBar / Add item

---
+++ app/src/main/java/com/example/househomey/HomeFragment.java
@@ -0,0 +1,18 @@
+package com.example.househomey;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the fragment's layout
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- app/src/main/java/com/example/househomey/MainActivity.java
+++ app/src/main/java/com/example/househomey/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.househomey;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -27,11 +32,43 @@ public class MainActivity extends AppCompatActivity implements FirestoreUpdateLi
         itemAdapter = new ItemAdapter(this, user.getItemList().getItems());
         itemListView.setAdapter(itemAdapter);
 
+        // Init home fragment
+        navigateToFragment(new HomeFragment());
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            Fragment fragment = null;
+            if (id == R.id.action_home) {
+                // Go to Home page
+                fragment = new HomeFragment();
+            } else if (id == R.id.action_add) {
+                // Go to Add Item page
+                fragment = new AddItemFragment();
+            } else {
+                // FIXME: Go to Profile Page
+                fragment = new HomeFragment();
+            }
+            navigateToFragment(fragment);
+            return true;
+        });
     }
 
     @Override
     public void notifyDataSetChanged() {
         itemAdapter.notifyDataSetChanged();
+    }
+
+    private void navigateToFragment(Fragment fragment) {
+        // Get the FragmentManager and start a transaction
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        // Replace the current fragment with the addItemFragment
+        transaction.replace(R.id.fragmentContainer, fragment);
+
+        // Commit the transaction
+        transaction.addToBackStack(null); // Optional, to add the transaction to the back stack
+        transaction.commit();
     }
 }
