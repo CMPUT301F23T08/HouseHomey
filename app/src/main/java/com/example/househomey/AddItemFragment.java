7c102eda426f1b86f348317e83a497dece82b30e
owencooke <ocooke@ualberta.ca>
2023-11-02 20:13:58 -0600

    Finished basic UI for Add Item (desc, date, cost)

--- app/src/main/java/com/example/househomey/AddItemFragment.java
+++ app/src/main/java/com/example/househomey/AddItemFragment.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputEditText;
+
 
 public class AddItemFragment extends Fragment {
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the fragment's layout
+    private TextInputEditText dateTextView;
 
-        return inflater.inflate(R.layout.fragment_add_item, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_add_item, container, false);
+        // Show DatePicker when date field selected
+        dateTextView = rootView.findViewById(R.id.add_item_date);
+        dateTextView.setOnClickListener(v -> showDatePicker());
+        return rootView;
+    }
+
+    private void showDatePicker() {
+        MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
+                .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+                .build();
+
+        datePicker.addOnPositiveButtonClickListener(selection -> dateTextView.setText(datePicker.getHeaderText()));
+
+        datePicker.show(getParentFragmentManager(), "Date Picker");
     }
 }
