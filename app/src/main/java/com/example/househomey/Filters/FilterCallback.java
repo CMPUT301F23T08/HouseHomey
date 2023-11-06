dfac2c05736cee35ccc1ac0372e3bb887965e91d
Jared Drueco <drueco03@gmail.com>
2023-11-06 09:52:49 -0700

    docs: added an @see on FilterCallback doc

--- app/src/main/java/com/example/househomey/Filters/FilterCallback.java
+++ app/src/main/java/com/example/househomey/Filters/FilterCallback.java
@@ -3,6 +3,7 @@ package com.example.househomey.Filters;
 
 /**
  * Callback interface for handling filter changes between fragments.
+ * @see com.example.househomey.HomeFragment
  */
 public interface FilterCallback {
     /**
