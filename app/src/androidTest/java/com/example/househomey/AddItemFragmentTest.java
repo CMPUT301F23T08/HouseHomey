22b072769c4a0c87aa14ce9a97ee2b63968decdd
owencooke <ocooke@ualberta.ca>
2023-11-08 11:22:11 -0700

    finish add valid item Espresso test
    Fixed an issue where the date being displayed on homepage for items was being affected by timezone

--- app/src/androidTest/java/com/example/househomey/AddItemFragmentTest.java
+++ app/src/androidTest/java/com/example/househomey/AddItemFragmentTest.java
@@ -19,9 +19,8 @@ import com.example.househomey.testUtils.TestSetup;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class AddItemFragmentTest extends TestSetup {
     @Before
@@ -29,42 +28,49 @@ public class AddItemFragmentTest extends TestSetup {
         onView(withId(R.id.action_add)).perform(click());
     }
 
-    public void selectFirstDayOfMonth() {
-        // Open date picker
+    public String selectFirstDayOfMonth() {
         onView(withId(R.id.add_item_date)).perform(click());
-        // Get the first day of this month
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        Date firstDayOfMonth = calendar.getTime();
-        // Format the date to match the MaterialDatePicker content description format
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d");
-        String formattedDate = dateFormat.format(firstDayOfMonth);
+        // Get the first day of the month matching MaterialDatePicker content description format
+        LocalDate currentDate = LocalDate.now().withDayOfMonth(1);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("EEEE, MMMM d");
+        String formattedDate = currentDate.format(dateFormat);
         // Select the day and click confirm
         onView(withContentDescription(formattedDate))
                 .inRoot(RootMatchers.isDialog())
                 .perform(click());
         onView(withId(com.google.android.material.R.id.confirm_button)).perform(click());
+        // Return HomePage's formatted date
+        return currentDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Test
     public void testAddItemWithValidData() {
-        String itemDescription = "Test Item";
-        // Add brief description
-        onView(withId(R.id.add_item_description)).perform(replaceText(itemDescription), closeSoftKeyboard());
         // Add acquisition date
-        selectFirstDayOfMonth();
-        // Add an estimated cost
-        onView(withId(R.id.add_item_cost)).perform(replaceText("100"), closeSoftKeyboard());
-        // TODO: Add other relevant fields
+        String acquisitionDate = selectFirstDayOfMonth();
+        // Add required description and estimated cost
+        String itemDescription = "Test Item";
+        String estimatedCost = "99.99";
+        onView(withId(R.id.add_item_description)).perform(replaceText(itemDescription), closeSoftKeyboard());
+        onView(withId(R.id.add_item_cost)).perform(replaceText(estimatedCost), closeSoftKeyboard());
+        // Add values to other non-required fields
+        onView(withId(R.id.add_item_make)).perform(replaceText("MyMake"), closeSoftKeyboard());
+        onView(withId(R.id.add_item_model)).perform(replaceText("MyModel"), closeSoftKeyboard());
+        onView(withId(R.id.add_item_serial_number)).perform(replaceText("1234567890"), closeSoftKeyboard());
+        onView(withId(R.id.add_item_comment)).perform(replaceText("this is a comment"), closeSoftKeyboard());
         // Click the confirm button to add the item
         onView(withId(R.id.add_item_confirm_button)).perform(click());
         // Check that we switch back to home page
         waitForView(withId(R.id.item_list));
-        // Check that the item is present in the list
+        // Check that the item in list matches description, date, and cost
         onData(anything())
                 .inAdapterView(withId(R.id.item_list))
                 .atPosition(0)
                 .onChildView(withId(R.id.item_description_text))
                 .check(matches(withText(itemDescription)));
+        onData(anything())
+                .inAdapterView(withId(R.id.item_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.item_text))
+                .check(matches(withText(acquisitionDate + " | $" + estimatedCost)));
     }
 }
