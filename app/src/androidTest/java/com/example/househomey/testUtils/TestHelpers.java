9d7a8c6a051b3e8202e751eefe1c87be899f36c7
owencooke <ocooke@ualberta.ca>
2023-11-08 16:40:00 -0700

    add JavaDocs to helpers for team reference

--- app/src/androidTest/java/com/example/househomey/testUtils/TestHelpers.java
+++ app/src/androidTest/java/com/example/househomey/testUtils/TestHelpers.java
@@ -18,10 +18,19 @@ import com.example.househomey.R;
 
 import org.hamcrest.Matcher;
 
+/**
+ * Helper methods for Espresso testing in Android applications.
+ * @author Owen Cooke
+ */
 public class TestHelpers {
     private static final long TIMEOUT = 5000; // Timeout in milliseconds
     private static final long POLLING_INTERVAL = 500; // Polling interval in milliseconds
 
+    /**
+     * Wait for a view to be displayed within a specified timeout (5s).
+     *
+     * @param viewMatcher The Matcher for the view to wait for.
+     */
     public static void waitForView(Matcher<View> viewMatcher) {
         long startTime = System.currentTimeMillis();
         long elapsedTime = 0;
@@ -47,10 +56,23 @@ public class TestHelpers {
         }
     }
 
+    /**
+     * Check if the list of Items has the expected number of Items.
+     *
+     * @param expectedCount The expected number of child elements in the list view.
+     */
     public static void hasListLength(int expectedCount) {
         onView(withId(R.id.item_list)).check(matches(hasChildCount(expectedCount)));
     }
 
+    /**
+     * Types text into an EditText field.
+     * Presses the IME action button, for actions like "Done," "Search," or "Next"
+     * that follow entering text. Also ensures the keyboard is closed.
+     *
+     * @param viewId The resource ID of the EditText view.
+     * @param text   The text to be entered into the view.
+     */
     public static void enterText(@IdRes int viewId, String text) {
         onView(withId(viewId)).perform(typeText(text), pressImeActionButton(), closeSoftKeyboard());
     }
