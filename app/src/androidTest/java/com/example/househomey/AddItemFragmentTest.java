d55b62e871625e19e49b1b7918a38d1af43c00f6
Owen Cooke <90405643+owencooke@users.noreply.github.com>
2023-11-09 11:46:35 -0700

    Finish Espresso tests for Add Item Fragment and Added some Espresso Helper Functions (#54)
    * finish add valid item Espresso test
    Fixed an issue where the date being displayed on homepage for items was being affected by timezone
    
    * Complete Add Item Fragment Espresso tests
    Add repeated Espresso functions to a helpers class
    
    * add JavaDocs to helpers for team reference
    
    * Fixing test for CI/CD
    
    * trying to fix test for CI/CD
    
    * trying adding Gradle flags and removing the comment test to see if it passes
    
    * Update android.yml
    
    * device change
    
    * Update android.yml
    
    * Update android.yml
    
    * fix scroll behaviour for fragments
    
    * change view item button sizes to fit on small screens
    
    * added the ability to create a unique Firebase user for required Espresso tests
    
    ---------
    
    Co-authored-by: ldbonkowski <lbonkows@ualberta.ca>

--- app/src/androidTest/java/com/example/househomey/AddItemFragmentTest.java
+++ app/src/androidTest/java/com/example/househomey/AddItemFragmentTest.java
@@ -3,13 +3,15 @@ package com.example.househomey;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.example.househomey.testUtils.EspressoWait.waitForView;
+import static com.example.househomey.testUtils.TestHelpers.enterText;
+import static com.example.househomey.testUtils.TestHelpers.hasListLength;
+import static com.example.househomey.testUtils.TestHelpers.waitForView;
 import static org.hamcrest.CoreMatchers.anything;
 
 import androidx.test.espresso.matcher.RootMatchers;
@@ -19,9 +21,8 @@ import com.example.househomey.testUtils.TestSetup;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class AddItemFragmentTest extends TestSetup {
     @Before
@@ -29,42 +30,77 @@ public class AddItemFragmentTest extends TestSetup {
         onView(withId(R.id.action_add)).perform(click());
     }
 
-    public void selectFirstDayOfMonth() {
-        // Open date picker
+    public String selectFirstDayOfMonth() {
         onView(withId(R.id.add_item_date)).perform(click());
-        // Get the first day of this month
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        Date firstDayOfMonth = calendar.getTime();
-        // Format the date to match the MaterialDatePicker content description format
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d");
-        String formattedDate = dateFormat.format(firstDayOfMonth);
+        // Get the first day of the month matching MaterialDatePicker content description format
+        LocalDate currentDate = LocalDate.now().withDayOfMonth(1);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("EEEE, MMMM d");
+        String formattedDate = currentDate.format(dateFormat);
         // Select the day and click confirm
         onView(withContentDescription(formattedDate))
                 .inRoot(RootMatchers.isDialog())
                 .perform(click());
         onView(withId(com.google.android.material.R.id.confirm_button)).perform(click());
+        // Return HomePage's formatted date
+        return currentDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Test
-    public void testAddItemWithValidData() {
-        String itemDescription = "Test Item";
-        // Add brief description
-        onView(withId(R.id.add_item_description)).perform(replaceText(itemDescription), closeSoftKeyboard());
+    public void testAddItemWithNewUser() {
         // Add acquisition date
-        selectFirstDayOfMonth();
-        // Add an estimated cost
-        onView(withId(R.id.add_item_cost)).perform(replaceText("100"), closeSoftKeyboard());
-        // TODO: Add other relevant fields
+        String acquisitionDate = selectFirstDayOfMonth();
+        // Add required description and estimated cost
+        String itemDescription = "Test Item";
+        String estimatedCost = "99.99";
+        enterText(R.id.add_item_description, itemDescription);
+        enterText(R.id.add_item_cost, estimatedCost);
+        // Add values to other non-required fields
+        enterText(R.id.add_item_make, "MyMake");
+        enterText(R.id.add_item_model, "MyModel");
+        enterText(R.id.add_item_serial_number, "1234567890");
+        onView(withId(R.id.add_item_comment)).perform(scrollTo());
+        waitForView(withId(R.id.add_item_comment));
+        enterText(R.id.add_item_comment, "this is a comment");
         // Click the confirm button to add the item
         onView(withId(R.id.add_item_confirm_button)).perform(click());
         // Check that we switch back to home page
         waitForView(withId(R.id.item_list));
-        // Check that the item is present in the list
+        hasListLength(1);
+        // Check that the item in list matches description, date, and cost
         onData(anything())
                 .inAdapterView(withId(R.id.item_list))
                 .atPosition(0)
                 .onChildView(withId(R.id.item_description_text))
                 .check(matches(withText(itemDescription)));
+        onData(anything())
+                .inAdapterView(withId(R.id.item_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.item_text))
+                .check(matches(withText(acquisitionDate + " | $" + estimatedCost)));
+    }
+
+    @Test
+    public void testSubmitWithRequiredEmpty() {
+        String defaultErrorMsg = "This field is required";
+        // Click the add button with empty form
+        onView(withId(R.id.add_item_confirm_button)).perform(click());
+        // Check that error messages are displayed in TextInputLayouts for required fields
+        onView(withId(R.id.add_item_description_layout)).check(matches(hasDescendant(withText(defaultErrorMsg))));
+        onView(withId(R.id.add_item_cost_layout)).check(matches(hasDescendant(withText(defaultErrorMsg))));
+        onView(withId(R.id.add_item_date_layout)).check(matches(hasDescendant(withText(defaultErrorMsg))));
+    }
+
+    @Test
+    public void testBackButtonGoesToHomePage() {
+        onView(withId(R.id.add_item_back_button)).perform(click());
+        // Check that we switch back to home page with empty list
+        waitForView(withId(R.id.item_list));
+        hasListLength(0);
+    }
+
+    @Test
+    public void testRoundCostTo2Decimals() {
+        enterText(R.id.add_item_cost, "99.9852323324");
+        onView(withId(R.id.add_item_cost)).check(matches(withText("99.99")));
     }
 }
