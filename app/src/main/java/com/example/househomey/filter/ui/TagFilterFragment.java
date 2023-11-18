1864147a4a8bef58ba264bb743437ad47d72ccc4
Jared Drueco <drueco03@gmail.com>
2023-11-18 13:53:33 -0700

    docs: added java docs for tag filter fragment

--- app/src/main/java/com/example/househomey/filter/ui/TagFilterFragment.java
+++ app/src/main/java/com/example/househomey/filter/ui/TagFilterFragment.java
@@ -1,3 +1,7 @@
+/**
+ * {@code TagFilterFragment} is a subclass of {@link FilterFragment} that provides filtering
+ * functionality based on tags using {@link TagFilter}.
+ */
 package com.example.househomey.filter.ui;
 
 import android.content.Context;
@@ -15,17 +19,40 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * This class represents a fragment for tag-based filtering. It extends {@link FilterFragment}
+ * and implements methods to manage and interact with tag filters.
+ * @author Jared Drueco
+ */
 public class TagFilterFragment extends FilterFragment {
     protected TagFilter tagFilter;
     private final ChipGroup tagChipGroup;
     private Map<String, Boolean> tagSelectionMap = new HashMap<>();
 
+    /**
+     * Constructs a new {@code TagFilterFragment} with the given parameters.
+     *
+     * @param title           The title of the fragment.
+     * @param contentView     The content view of the fragment.
+     * @param filterCallback  The callback for filter actions.
+     * @param itemList        The list of items to be filtered.
+     */
     public TagFilterFragment(String title, View contentView, FilterCallback filterCallback, ArrayList<Item> itemList) {
         super(title, contentView, filterCallback);
         tagChipGroup = contentView.findViewById(R.id.chip_group_labels);
         populateTagChipGroup(itemList);
     }
 
+    /**
+     * Constructs a new {@code TagFilterFragment} with the given parameters, including a
+     * pre-existing {@link TagFilter}.
+     *
+     * @param title           The title of the fragment.
+     * @param contentView     The content view of the fragment.
+     * @param filterCallback  The callback for filter actions.
+     * @param itemList        The list of items to be filtered.
+     * @param tagFilter       The pre-existing tag filter.
+     */
     public TagFilterFragment(String title, View contentView, FilterCallback filterCallback, ArrayList<Item> itemList, TagFilter tagFilter) {
         super(title, contentView, filterCallback);
         this.tagFilter = tagFilter;
@@ -35,6 +62,11 @@ public class TagFilterFragment extends FilterFragment {
         populateTagChipGroup(itemList);
     }
 
+    /**
+     * Populates the tag {@link ChipGroup} with tags from the provided item list.
+     *
+     * @param itemList The list of items containing tags.
+     */
     private void populateTagChipGroup(ArrayList<Item> itemList) {
         Set<String> existingTags = new HashSet<>();
         for (Item item : itemList) {
@@ -47,6 +79,12 @@ public class TagFilterFragment extends FilterFragment {
         }
     }
 
+    /**
+     * Creates a new {@link Chip} for the given tag.
+     *
+     * @param tag The tag for which the chip is created.
+     * @return The created chip.
+     */
     private Chip createTagChip(String tag) {
         Context context = contentView.getContext();
         Chip chip = new Chip(context);
@@ -60,6 +98,12 @@ public class TagFilterFragment extends FilterFragment {
         return chip;
     }
 
+    /**
+     * Autofills the last selected state of the chip based on the tagSelectionMap.
+     *
+     * @param chip The chip to be autofilled.
+     * @param tag  The tag associated with the chip.
+     */
     private void autofillLastSelectedChip(Chip chip, String tag) {
         boolean isSelected = tagSelectionMap.containsKey(tag) && Boolean.TRUE.equals(tagSelectionMap.get(tag));
         chip.setChecked(isSelected);
@@ -67,6 +111,12 @@ public class TagFilterFragment extends FilterFragment {
         tagSelectionMap.put(tag, isSelected);
     }
 
+    /**
+     * Sets the {@link Chip} listener to handle checked state changes.
+     *
+     * @param chip The chip to set the listener for.
+     * @param tag  The tag associated with the chip.
+     */
     private void setChipListener(Chip chip, String tag) {
         chip.setOnCheckedChangeListener((buttonView, isChecked) -> {
             tagSelectionMap.put(tag, isChecked);
@@ -74,6 +124,9 @@ public class TagFilterFragment extends FilterFragment {
         });
     }
 
+    /**
+     * Gets the filter input, creates a {@link TagFilter}, and notifies the callback.
+     */
     @Override
     public void getFilterInput() {
         this.tagFilter = new TagFilter(this.tagSelectionMap);
@@ -81,6 +134,9 @@ public class TagFilterFragment extends FilterFragment {
         dismiss();
     }
 
+    /**
+     * Resets the filter to its initial state and notifies the callback.
+     */
     @Override
     public void resetFilter() {
         filterCallback.onFilterReset(this.tagFilter);
