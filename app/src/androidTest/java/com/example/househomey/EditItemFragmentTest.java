bd1f848b7b29ca2bd997b401460e9af5ae86e8ae
owencooke <ocooke@ualberta.ca>
2023-11-27 18:58:54 -0700

    add Espresso test for deleting a photo from Edit Item page

--- app/src/androidTest/java/com/example/househomey/EditItemFragmentTest.java
+++ app/src/androidTest/java/com/example/househomey/EditItemFragmentTest.java
@@ -5,13 +5,16 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.example.househomey.testUtils.TestHelpers.enterText;
 import static com.example.househomey.testUtils.TestHelpers.waitFor;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.not;
 
 import com.example.househomey.testUtils.TestSetup;
 import com.google.common.collect.ImmutableMap;
@@ -39,11 +42,13 @@ public class EditItemFragmentTest extends TestSetup {
         // Create mock initial item in DB
         database.addTestItem(mockData);
         // Click to view the item page
-        onData(anything())
-                .inAdapterView(withId(R.id.item_list))
-                .atPosition(0)
-                .onChildView(withId(R.id.action_view))
-                .perform(click());
+        waitFor(() -> {
+            onData(anything())
+                    .inAdapterView(withId(R.id.item_list))
+                    .atPosition(0)
+                    .onChildView(withId(R.id.action_view))
+                    .perform(click());
+        });
         // Click on edit button
         onView(withId(R.id.edit_button)).perform(click());
     }
@@ -88,5 +93,18 @@ public class EditItemFragmentTest extends TestSetup {
         onView(withId(R.id.add_item_description_layout)).check(matches(hasDescendant(withText(defaultErrorMsg))));
         onView(withId(R.id.add_item_cost_layout)).check(matches(hasDescendant(withText(defaultErrorMsg))));
         onView(withId(R.id.add_item_date_layout)).check(matches(hasDescendant(withText(defaultErrorMsg))));
+    }
+
+    @Test
+    public void testDeleteExistingPhoto() {
+        // Check that there is a photo present
+        onView(withId(R.id.add_photo_grid)).check(matches(hasChildCount(2)));
+
+        // Click the delete button on the photo
+        onView(withId(R.id.delete_photo_button)).perform(click());
+
+        // Check that photo was deleted from the gallery
+        onView(withId(R.id.add_photo_grid)).check(matches(hasChildCount(1)));
+        onView(withId(R.id.add_photo_grid)).check(matches(not((hasDescendant(allOf(withId(R.id.gallery_image_view), isDisplayed()))))));
     }
 }
