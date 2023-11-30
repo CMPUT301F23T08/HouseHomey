532a16c26a9230f91a17ab607dc9979695778d54
ldbonkowski <lbonkows@ualberta.ca>
2023-11-30 00:50:19 -0700

    Javadoc

--- app/src/main/java/com/example/househomey/sort/TagComparator.java
+++ app/src/main/java/com/example/househomey/sort/TagComparator.java
@@ -7,6 +7,12 @@ import java.util.Comparator;
 import java.util.Set;
 
 public class TagComparator implements Comparator<Item> {
+    /**
+     * Compares the first tag of each item
+     * @param item1 the first object to be compared.
+     * @param item2 the second object to be compared.
+     * @return 1 if o1 is o2, 0 if tied, -1 otherwise
+     */
     @Override
     public int compare(Item item1, Item item2) {
         Tag tag1 = getFirstTag(item1);
