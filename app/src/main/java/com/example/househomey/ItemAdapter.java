20f723f1063a7cafc798266fce0c09f34d45fa56
Jared Drueco <drueco03@gmail.com>
2023-12-02 15:22:57 -0700

    fix: empty make still has separator

--- app/src/main/java/com/example/househomey/ItemAdapter.java
+++ app/src/main/java/com/example/househomey/ItemAdapter.java
@@ -102,7 +102,7 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         Set<Tag> itemTags = item.getTags();
         ((TextView) view.findViewById(R.id.item_extra_tags_text)).setText("");
         if (itemTags.size() > 0) {
-            makeString += " | ";
+            if (makeString != "") makeString += " | ";
             Chip chip = FragmentUtils.makeChip(itemTags.iterator().next().getTagLabel(), false, chipGroup, view.getContext(), R.color.white, R.color.black, R.color.black);
             chip.setFocusable(false);
             chip.setClickable(false);
