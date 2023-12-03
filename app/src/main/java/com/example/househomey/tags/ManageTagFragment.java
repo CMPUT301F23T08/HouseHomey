73f0636d935ec0ba856875ebde553985ffde36a0
Matthew Neufeld <118775633+MatthewNeufeld@users.noreply.github.com>
2023-12-03 11:56:21 -0700

    Tags + keywords -> case insensitive &  can't add the same keyword more than once (#115)
    * Added unit tests for date filter
    
    * made the fix
    
    * applied util to keywords and tags and made it so the same keyword can't be added more than once

--- app/src/main/java/com/example/househomey/tags/ManageTagFragment.java
+++ app/src/main/java/com/example/househomey/tags/ManageTagFragment.java
@@ -87,6 +87,7 @@ public class ManageTagFragment extends TagFragment {
      * @param tagLabel the label that will go on the tag
      */
     private void addTag(String tagLabel) {
+        tagLabel = FragmentUtils.initialCase(tagLabel);
         if (!tagList.stream()
                 .map(Tag::getTagLabel)
                 .collect(Collectors.toList()).contains(tagLabel)) {
